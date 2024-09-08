72118e944249197999c0d739c4560e045c312363
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2024-09-09 00:53:18 +0530

    adding authentication middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import { ApiError } from "../utils/ApiError";
+import {asyncHandler} from "../utils/asyncHandler.js";
+
+
+export const verifyJwt = asyncHandler(async (req, _, next) => {
+
+    try {
+
+        const token = req.cookies?.accesstoken || req.headers("Authorization")?.replace("Bearer ", "");
+
+        if(!token) throw new ApiError(401, "unauthorized request");
+
+        const decodedToken = jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET);
+
+        const user = decodedToken;
+        
+        req.user = user;
+
+        next();
+
+    } catch (error) {
+
+        throw new ApiError(401, error?.message || "Invalid access token");
+    }
+})
